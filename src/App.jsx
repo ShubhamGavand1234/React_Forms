0321ca9d17d68d674acd822dec86d0957bc737dd
ShubhamGavand1234 <shubhamgavand@yahoo.in>
2024-07-31 16:46:58 +0530

    value Change on click

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,28 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [name, setName] = useState("");
+  const [headingText, setHeadingText] = useState("");
+
+  function handleChange(event) {
+    setName(event.target.value);
+  }
+
+  function handleClick() {
+    setHeadingText(name);
+  }
 
   return (
-    <>
-      <h1>Hi</h1>
-    </>
+    <div className="container">
+      <h1>Hello {headingText} </h1>
+      <input
+        onChange={handleChange}
+        type="text"
+        placeholder="What's your name?"
+        value={name}
+      />
+      <button onClick={handleClick}>Submit</button>
+    </div>
   );
 }
 
